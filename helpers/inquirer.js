80934a5875739d7db3f5e71e841ed875236fb923
Ginnet <ginrodriguezrojas@hotmail.com>
2022-12-07 16:28:39 -0500

    primer commit

---
+++ helpers/inquirer.js
@@ -0,0 +1,146 @@
+import inquirer from 'inquirer';
+import colors from 'colors';
+
+
+const preguntas = [
+    {
+        type: 'list',
+        name: 'opcion',
+        message: 'Seleccione una opción',
+        choices: [
+            {
+                value: 1,
+                name: `${"1.".red} Buscar cuidad`,
+            },
+            {
+                value: 2,
+                name: `${"2.".red} Historial`,
+            },
+            {
+                value: 0,
+                name: `${"0.".red}.Salir `
+            },
+
+        ],
+    },
+];
+
+//imprimir el menu
+const inquirerMenu = async () => {
+    console.clear();
+    console.log("=========================".gray);
+    console.log("Seleccione una opción".white);
+    console.log("=========================\n".gray);
+
+    const { opcion } = await inquirer.prompt(preguntas);
+    return opcion;
+};
+
+const pausa = async () => {
+    const question = [
+        {
+            type: "input",
+            name: "enter",
+            message: `Presione ${"ENTER".grey} para continuar.`
+        }
+    ];
+
+    console.log("\n")
+
+    await inquirer.prompt(question)
+
+}
+const leerInput = async (message) => {
+    const question = [
+        {
+            type: "input",
+            name: "desc",
+            message,
+            validate(value) {
+                if (value.length === 0) {
+                    return "Por favor ingresar un valor"
+                }
+                return true;
+
+            }
+
+        }
+    ];
+
+    const { desc } = await inquirer.prompt(question);
+    return desc;
+}
+const listarLugares = async (lugares = []) => {
+
+    const choices = lugares.map((lugar, i) => {
+        const idx = `${ i + 1}`.green;
+        return {
+            value: lugar.id,
+            name: `${idx} ${lugar.nombre}`
+        }
+    });
+    choices.unshift({
+        value:"0",
+        name:"0".green + " " +"Cancelar"
+    })
+
+    const preguntas = [
+        {
+            type: "list",
+            name: "id",
+            message: "Seleccione lugar: ",
+            choices
+        }
+    ];
+
+    const { id } = await inquirer.prompt(preguntas);
+    return id;
+}
+//const confirmar = async (message) => {
+//    const question = [
+//        {
+//            type: "confirm",
+//            name: "ok",
+//            message
+//
+//        }
+//    ];
+//    const { ok } = await inquirer.prompt(question);
+//    return ok;
+//}
+//
+//const mostrarListadoChecklist = async (tareas = []) => {
+//
+//    const choices = tareas.map((tarea, i) => {
+//
+//        const idx = `${ i + 1}`.green;
+//
+//        return {
+//            value: tarea.id,
+//            name: `${idx} ${tarea.desc}`,
+//            checked: (tarea.completadoEn) ? true : false 
+//        }
+//    });
+//   
+//    const pregunta = [
+//        {
+//            type: "checkbox",
+//            name: "ids",
+//            message: "Selecciones",
+//            choices
+//        }
+//    ]
+//
+//    const { ids } = await inquirer.prompt(pregunta);
+//    return ids;
+//}
+//
+//
+export {
+    inquirerMenu,
+    pausa,
+    leerInput,
+    listarLugares,
+//    confirmar,
+//    mostrarListadoChecklist
+}
---
+++ models/busquedas.js
@@ -0,0 +1,116 @@
+import fs from "fs";
+import axios from 'axios';
+
+class Busquedas {
+    historial = [];
+    dbPath = './db/database.json'
+
+    constructor() {
+        // TODO:  leer BD si existe 
+        this.leerBD();
+    }
+
+    get historialCapitalizado(){
+
+        return this.historial.map(lugar=>{
+            let palabra = lugar.split(" ");
+            palabra = palabra.map(p => p[0].toUpperCase() + p.substring(1));
+
+            return palabra.join("");
+        })
+    }
+
+    get ParamsMapbox() {
+        return {
+            "access_token": process.env.MAPBOX_key,
+            "limit": 5,
+            "language": "es"
+        }
+    }
+    get ParamsOPENWEATHER() {
+        return {
+            "appid": process.env.OPENWEATHER_KEY,
+            "lang": "es",
+            "units": "metric",
+        }
+    }
+
+
+    async ciudad(lugar = "") {
+
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.ParamsMapbox
+
+            });
+            const resp = await instance.get();
+            return resp.data.features.map(lugar => ({
+                id: lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1]
+            }));
+        } catch (error) {
+            return [];
+
+        }
+    }
+
+    async climaLugar(lat, lon) {
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather?`,
+                params: { ...this.ParamsOPENWEATHER, lat, lon }
+            })
+            const resp = await instance.get();
+            const { weather, main } = resp.data
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+
+        } catch (error) {
+            return [];
+
+        }
+    }
+
+    agregarHistorial(lugar = "") {
+        if (this.historial.includes(lugar.toLocaleLowerCase())) {
+            return;
+        }
+        this.historial = this.historial.splice(0,5);
+        this.historial.unshift(lugar.toLocaleLowerCase());
+
+        this.guardarBD();
+
+    }
+    guardarBD() {
+
+        const payload = {
+            historial: this.historial
+
+        }
+
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload))
+
+    }
+
+
+
+    leerBD() {
+        if(!fs.existsSync(this.dbPath)) return;
+
+        const info =fs.readFileSync(this.dbPath,{encoding: "utf-8"});
+        const data = JSON.parse(info);
+
+        this.historial = data.historial
+
+
+    }
+}
+
+export { Busquedas }
